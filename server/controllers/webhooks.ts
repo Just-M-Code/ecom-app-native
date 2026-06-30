e0057fffa67176f90dadd3a72e61c652b5d1f426
Just-M-Code <web3rabbithole@gmail.com>
2026-06-30 21:21:38 +0200

    clerk webhooks endpoint

--- server/controllers/webhooks.ts
+++ server/controllers/webhooks.ts
@@ -1,20 +1,29 @@
 import { verifyWebhook } from "@clerk/express/webhooks";
 import { Request, Response } from "express";
+import User from "../models/User.js";
 
 export const clerkWebhook = async (req: Request, res: Response) => {
   try {
     const evt = await verifyWebhook(req);
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data;
-    const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`,
-    );
-    console.log("Webhook payload:", evt.data);
+    if (evt.type === "user.created" || evt.type === "user.updated") {
+      const user = await User.findOne({ clerkId: evt.data.id });
 
-    return res.send("Webhook received");
+      const userData = {
+        clerkId: evt.data.id,
+        email: evt.data?.email_addresses[0]?.email_address,
+        name: evt.data?.first_name + " " + evt.data?.last_name,
+        image: evt.data?.image_url,
+      };
+
+      if (user) {
+        await User.findOneAndUpdate({ clerkId: evt.data.id }, userData);
+      } else {
+        await User.create(userData);
+      }
+    }
+
+    return res.json({ success: true, message: "Webhook received" });
   } catch (err) {
     console.error("Error verifying webhook:", err);
     return res.status(400).send("Error verifying webhook");
